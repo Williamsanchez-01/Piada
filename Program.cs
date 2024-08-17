3b280befaf913614dd8dcf5cfca29ccc41840757
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-17 05:29:15 -0300

    Finaliza Exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Em uma padaria");
+
+Console.WriteLine("-Quanto é o cafézinho?");
+
+Console.WriteLine("-2 reais ");
+
+Console.WriteLine("-E o açúcar?");
+
+Console.WriteLine("-O açúcar a gente nao cobra.");
+
+Console.WriteLine("-Então pode me ver 2 quilos.");
+
+Console.WriteLine("Por favor.");
